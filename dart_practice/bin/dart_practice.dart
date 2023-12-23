dbb54eb04671249ed6261d9af1d402c06bd79634
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2023-12-23 16:54:40 +0600

    [DONE] comment

--- dart_practice/bin/dart_practice.dart
+++ dart_practice/bin/dart_practice.dart
@@ -1,5 +1,38 @@
 void main(){
   
-  print("Hello Dart");
+  print("Hello World!");
+
+  // This is Single Line Comment
+
+  /*
+
+  This is Multiple line Comment
+  By "Mohammad Sumon"
+   */
+
+  // var x= 10;
+  // var y= 40;
+  // var z = x+y;
+  // print(x);
+  // print(y);
+  // print(z);
+  //Dart List
+  var city=["Dhaka","Khulna", "Chitagong", "Rajshahi",12, 10, 26.1 , false, true];
+
+  print(city[7]);
+
+  var student = {
+    'name': 'Mohammad',
+    'age': 24,
+    'Group' :'Science',
+    'City' :'Dhaka',
+    'Hometown' :' Bhola',
+  };
+  print(student['City']);
+
+  // 'key' : 'value'
+  // 'name' : 'mohammad'
+
+
   
 }

35c91d1f8c3cd734ac5f16c8c6130246a6ece404
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2023-12-30 09:01:11 +0600

    Small Update

--- dart_practice/bin/dart_practice.dart
+++ dart_practice/bin/dart_practice.dart
@@ -8,12 +8,24 @@ void main(){
   // city.add("Barishal");
   print(city);
 
-  var numbers = [1 ,2 ,3 , 4, 5,6];
+  var numbers = [1 ,2 ,3 , 4, 5,6,7,8,9];
 
+  //Add
   //numbers.add(7);
   //numbers.addAll([7,8,9,10]);
+
+  //Insert
   //numbers.insert(3, 100);
   //numbers.insertAll(3,[100,200,300,400]);
-  
+
+  //Update
+  // numbers[1]=200;
+  // numbers[3]=400;
+
+  //Removing
+  //numbers.removeLast();
+  //numbers.removeAt(2);
+  numbers.remove(7);
+
   print(numbers);
 }

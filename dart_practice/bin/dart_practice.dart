e0c8b4cc92636984f8c8cd06ae14bb3fc310bcac
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-05-02 07:56:48 +0600

    Update

--- dart_practice/bin/dart_practice.dart
+++ dart_practice/bin/dart_practice.dart
@@ -1,31 +1,5 @@
-void main(){
+import 'package:dart_practice/dart_practice.dart' as dart_practice;
 
-  const city= ['Dhaka', 'Comilla', 'Rajshai'];
-
-  //print(city);
-
-  // city.add("Khulna");
-  // city.add("Barishal");
-  print(city);
-
-  var numbers = [1 ,2 ,3 , 4, 5,6,7,8,9];
-
-  //Add
-  //numbers.add(7);
-  //numbers.addAll([7,8,9,10]);
-
-  //Insert
-  //numbers.insert(3, 100);
-  //numbers.insertAll(3,[100,200,300,400]);
-
-  //Update
-  // numbers[1]=200;
-  // numbers[3]=400;
-
-  //Removing
-  //numbers.removeLast();
-  //numbers.removeAt(2);
-  numbers.remove(7);
-
-  print(numbers);
+void main(List<String> arguments) {
+  print('Hello world: ${dart_practice.calculate()}!');
 }

478c84edb20f2606b87cff29a2892db503a4a93e
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-05-13 12:28:15 +0600

    Operators

---
+++ dart_practice/test/arithmetic_operator.dart
@@ -0,0 +1,13 @@
+void main(){
+
+  var a=10;
+  var b=3;
+
+  print (a+b);
+  print(a-b);
+  print(a*b);
+  print(a/b);
+  print(a%b);
+  
+
+}
---
+++ dart_practice/test/unary_constants.dart
@@ -0,0 +1,34 @@
+void main(){
+
+
+  var x=10;
+  print(++x);
+  print(++x);
+  print(x++);
+  print(++x);
+  print(x++);
+  print(x++);
+  print(x++);
+  print(++x);
+  print(x++);
+
+  var y=10;
+  print(--y);
+  print(--y);
+  print(--y);
+  print(--y);
+  print(--y);
+  print(--y);
+
+  var z=10;
+  print(z--);
+  print(z--);
+  print(z--);
+  print(z--);
+  print(--z);
+  print(z--);
+  print(z--);
+  print(z--);
+
+
+}

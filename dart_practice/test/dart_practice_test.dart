acc5490049f89244462e71834d63af03638f650f
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-05-13 12:24:14 +0600

    Unary Operator

--- dart_practice/test/dart_practice_test.dart
+++ dart_practice/test/dart_practice_test.dart
@@ -1,11 +1,42 @@
 void main(){
 
-  var a=10;
-  var b=3;
+  // var a=10;
+  // var b=3;
+  //
+  // print (a+b);
+  // print(a-b);
+  // print(a*b);
+  // print(a/b);
+  // print(a%b);
 
-  print (a+b);
-  print(a-b);
-  print(a*b);
-  print(a/b);
-  print(a%b);
+  var x=10;
+  print(++x);
+  print(++x);
+  print(x++);
+  print(++x);
+  print(x++);
+  print(x++);
+  print(x++);
+  print(++x);
+  print(x++);
+
+  var y=10;
+  print(--y);
+  print(--y);
+  print(--y);
+  print(--y);
+  print(--y);
+  print(--y);
+
+  var z=10;
+  print(z--);
+  print(z--);
+  print(z--);
+  print(z--);
+  print(--z);
+  print(z--);
+  print(z--);
+  print(z--);
+
+
 }

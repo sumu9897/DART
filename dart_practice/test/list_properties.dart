c08b037b5dd5684b7315d78fd06e5ead1d257f99
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-05-13 12:35:44 +0600

    list properties

---
+++ dart_practice/test/list_properties.dart
@@ -0,0 +1,19 @@
+void main(){
+
+  var city =['Dhaka', 'Khulna', 'Rajshashi','barishal'];
+
+  var result = city.length;
+  var result1 = city.reversed;
+  var result2 = city.first;
+  var result3 = city.firstOrNull;
+  var result4 = city.hashCode;
+  var result5 = city.isEmpty;
+
+  print(result);
+  print(result1);
+  print(result2);
+  print(result3);
+  print(result4);
+  print(result5);
+
+}

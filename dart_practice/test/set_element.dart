6adf10b92583a8558136908751761726308c2dd8
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-05-13 13:40:21 +0600

    set element

--- dart_practice/test/set_element.dart
+++ dart_practice/test/set_element.dart
@@ -3,6 +3,8 @@ void main(){
   var myCitySet=<String>{'Dhaka','Barishal','Bhola','Comilla'};
   myCitySet.addAll({'Khulna','rajshahi'});
 
+  myCitySet.clear();
+
   print(myCitySet.elementAt(3));
 
 

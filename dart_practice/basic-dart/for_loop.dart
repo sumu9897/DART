af7a01910c590aaa04642b6d608f48e71bbab7a6
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-05-28 22:35:59 +0600

    For loop

---
+++ dart_practice/basic-dart/for_loop.dart
@@ -0,0 +1,11 @@
+void main(){
+  for (int i = 1; i<=10;i++){
+    greeting();
+  }
+}
+
+void greeting(){
+  print('Welcome to home');
+  print('Do you wanna take some tea/coffie');
+  print('Good Bye');
+}
